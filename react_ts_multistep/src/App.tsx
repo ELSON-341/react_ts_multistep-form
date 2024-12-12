e4433a158b276f1c4e6ef227af793a9b163a00f6
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-12-12 09:09:44 -0300

    parsa

--- react_ts_multistep/src/App.tsx
+++ react_ts_multistep/src/App.tsx
@@ -1,10 +1,40 @@
+// Components
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import ReviewForm from './components/ReviewForm'
+import Thanks from './components/Thanks'
+import UserForm from './components/UserForm'
+
+// Hooks
+
+// CSS
 import './App.css'
 
 function App() {
 
   return (
       <div className='app'>
-        <h1>Rect TS multistep form</h1>
+        <div className="header">
+          <h2>Deixe sua avaliação</h2>
+          <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
+        </div>
+        <div className="form-container">
+          <p>passos</p>
+          <form>
+            <div className="inputs-container">
+              <UserForm/>
+            </div>
+            <div className="actions">
+              <button>
+                <GrFormPrevious/>
+                <span>Voltar</span>
+              </button>
+              <button>
+                <span>avançar</span>
+                <GrFormNext/>
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
   )
 }
